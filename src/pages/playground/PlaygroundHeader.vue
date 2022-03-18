673111f1c56f65183f96d69ddd69ea1b954f5f9c
Xie Yuheng <xyheme@gmail.com>
2022-03-18 23:28:27 +0800

    up

--- src/pages/playground/PlaygroundHeader.vue
+++ src/pages/playground/PlaygroundHeader.vue
@@ -1,9 +1,13 @@
 <template>
   <div
-    to="/playground"
     class="flex items-center justify-between border-b border-rose-300 bg-rose-200 px-3 py-2"
   >
-    <div class="font-cute text-xl font-semibold text-rose-900">Playground</div>
+    <router-link
+      to="/playground"
+      class="font-cute text-xl font-semibold text-rose-900"
+    >
+      Playground
+    </router-link>
     <PlaygroundToolbar :state="state" />
   </div>
 </template>
