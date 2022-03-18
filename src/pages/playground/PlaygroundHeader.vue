5ba2e417ea9d573cce09876b42068b4cadf7e90b
Xie Yuheng <xyheme@gmail.com>
2022-03-18 17:15:34 +0800

    PlaygroundHeader

---
+++ src/pages/playground/PlaygroundHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center justify-between px-3 py-2 bg-rose-100">
+    <div class="font-cute font-semibold text-rose-900">Playground</div>
+    <div></div>
+  </div>
+</template>
+
+<script setup></script>
--- src/pages/playground/PlaygroundPage.vue
+++ src/pages/playground/PlaygroundPage.vue
@@ -1,14 +1,17 @@
 <template>
   <PageLayout>
-    <div class="flex h-full">
-      <div class="w-7/12">
-        <CodeEditor :state="state" />
-      </div>
-      <div v-if="state.error" class="w-6/12 border-l-2 border-rose-300">
-        {{ state.error.message }}
-      </div>
-      <div v-else class="w-6/12 border-l-2 border-rose-300">
-        <NetViewer :state="state" />
+    <div class="flex flex-col h-full">
+      <PlaygroundHeader />
+      <div class="flex h-full overflow-y-auto">
+        <div class="w-7/12">
+          <CodeEditor :state="state" />
+        </div>
+        <div v-if="state.error" class="w-6/12 border-l-2 border-rose-300">
+          {{ state.error.message }}
+        </div>
+        <div v-else class="w-6/12 border-l-2 border-rose-300">
+          <NetViewer :state="state" />
+        </div>
       </div>
     </div>
   </PageLayout>
@@ -21,6 +24,7 @@ import { PlaygroundState as State } from "./playground-state"
 import CodeEditor from "../../components/molecules/CodeEditor.vue"
 import NetViewer from "../../components/molecules/NetViewer.vue"
 import PageLayout from "../../components/layouts/page-layout/PageLayout.vue"
+import PlaygroundHeader from "./PlaygroundHeader.vue"
 
 const state = reactive(new State())
 
