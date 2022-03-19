eb0497d6c916f635483255982bcc5abb732a60c7
Xie Yuheng <xyheme@gmail.com>
2022-03-19 08:44:50 +0800

    up

--- src/pages/playground/PlaygroundEditor.vue
+++ src/pages/playground/PlaygroundEditor.vue
@@ -2,7 +2,7 @@
   <div class="h-full">
     <textarea
       spellcheck="false"
-      class="h-full w-full overflow-y-auto whitespace-pre p-3 font-mono text-base"
+      class="h-full w-full overflow-y-auto whitespace-pre p-3 font-mono text-sm md:text-base"
       v-model="state.text"
     ></textarea>
   </div>
