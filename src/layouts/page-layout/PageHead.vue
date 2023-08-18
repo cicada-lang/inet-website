445f036178f3f22308da46152670f9d5636366b0
Xie Yuheng <xyheme@gmail.com>
2023-08-18 22:14:47 +0800

    add `settings/` page
    - with simple `PageHead`

--- src/layouts/page-layout/PageHead.vue
+++ src/layouts/page-layout/PageHead.vue
@@ -1,43 +1,48 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import IconGitHub from '../../components/icons/IconGitHub.vue'
 import PageLogo from './PageLogo.vue'
 
 const route = useRoute()
 </script>
 
 <template>
-  <div class="flex items-baseline justify-between">
-    <div class="flex items-baseline pr-4">
-      <RouterLink to="/"> <PageLogo /> </RouterLink>
-    </div>
+  <div class="flex-wrap flex items-baseline">
+    <RouterLink to="/" class="pr-4"> <PageLogo /> </RouterLink>
 
-    <div class="flex items-baseline space-x-4">
-      <RouterLink
-        to="/playground"
-        class="text-xl hover:underline underline-offset-4 decoration-2"
-        :class="{
-          underline: route.path.startsWith('/playground'),
-        }"
-      >
-        Playground
-      </RouterLink>
+    <RouterLink
+      to="/playground"
+      class="pr-4 text-xl hover:underline underline-offset-4 decoration-2"
+      :class="{
+        underline: route.path.startsWith('/playground'),
+      }"
+    >
+      Playground
+    </RouterLink>
 
-      <a
-        target="_blank"
-        href="https://readonly.link/books/https://cdn.inet.cic.run/docs/books/programming-with-interaction-nets/book.json"
-        class="text-xl hover:underline underline-offset-4 decoration-2"
-      >
-        Docs
-      </a>
+    <a
+      target="_blank"
+      href="https://readonly.link/books/https://cdn.inet.cic.run/docs/books/programming-with-interaction-nets/book.json"
+      class="pr-4 text-xl hover:underline underline-offset-4 decoration-2"
+    >
+      Docs
+    </a>
 
-      <a
-        target="_blank"
-        href="https://github.com/cicada-lang/inet"
-        class="text-xl hover:underline underline-offset-4 decoration-2"
-      >
-        <IconGitHub class="w-5 h-5" />
-      </a>
-    </div>
+    <a
+      target="_blank"
+      href="https://github.com/cicada-lang/inet"
+      class="text-xl pr-4 hover:underline underline-offset-4 decoration-2"
+    >
+      GitHub
+    </a>
+
+    <RouterLink
+      to="/settings"
+      class="text-xl pr-4 flex items-center hover:underline underline-offset-4 decoration-2"
+      :class="{
+        underline: route.path.startsWith('/settings'),
+      }"
+    >
+      Settings
+    </RouterLink>
   </div>
 </template>
--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -1,9 +1,11 @@
 import { RouteRecordRaw } from 'vue-router'
 import Home from './home/Home.vue'
 import Playground from './playground/Playground.vue'
+import Settings from './settings/Settings.vue'
 
 export const routes: Array<RouteRecordRaw> = [
   { path: '/', component: Home },
+  { path: '/settings', component: Settings },
   {
     path: '/playground/:encoded?',
     component: Playground,
