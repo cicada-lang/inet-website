15fc1de418cc3ca498a015182e92a43c39e1877e
Xie Yuheng <xyheme@gmail.com>
2023-08-21 16:25:28 +0800

    [play] `renderStack`

--- src/components/play/renderStack.ts
+++ src/components/play/renderStack.ts
@@ -2,18 +2,24 @@ import { formatValue } from '@cicada-lang/inet/lib/lang/value/formatValue'
 import { State } from './State'
 
 export function renderStack(state: State): void {
+  state.ctx.save()
+
   for (const [i, value] of state.mod.env.stack.entries()) {
     const valueText = formatValue(value)
+    state.ctx.font = '18px sans-serif'
     const valueTextMetrics = state.ctx.measureText(valueText)
-    state.ctx.beginPath()
     const unitHeight = 36
     const x = 0
     const y = state.canvas.height - unitHeight * i
     const xPadding = 5
     const boxWdith = Math.max(100, valueTextMetrics.width + xPadding * 2)
     const boxHeight = unitHeight
+    state.ctx.beginPath()
+    state.ctx.strokeStyle = 'black'
+    state.ctx.lineWidth = 0.5
     state.ctx.strokeRect(x, y - unitHeight, boxWdith, boxHeight)
-    state.ctx.font = '18px sans-serif'
     state.ctx.fillText(valueText, x + xPadding, y - 12)
   }
+
+  state.ctx.restore()
 }
