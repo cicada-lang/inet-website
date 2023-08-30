3ca36ab57ca97a9b735c68963af4f3e275e946a6
Xie Yuheng <xyheme@gmail.com>
2023-08-30 13:39:02 +0800

    up

--- src/components/play/pages/types/selectFirstType.ts
+++ src/components/play/pages/types/selectFirstType.ts
@@ -1,9 +1,11 @@
 import { State } from '../../State'
+import { selectType } from './selectType'
 
 export function selectFirstType(state: State): void {
   for (const [name, definition] of state.mod.definitions) {
     if (definition['@kind'] === 'TypeDefinition') {
-      state.selectedType = { name, definition }
+      selectType(state, name)
+      return
     }
   }
 }
