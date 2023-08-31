f25507f5415eb212a6b7f965acccd9b62d4f44a7
Xie Yuheng <xyheme@gmail.com>
2023-09-01 01:25:00 +0800

    up

--- src/pages/playground/PlaygroundEditor.vue
+++ src/pages/playground/PlaygroundEditor.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <textarea
     spellcheck="false"
-    class="w-full overflow-y-auto whitespace-pre p-3 font-code bg-white dark:bg-black text-sm md:text-base"
+    class="w-full overflow-y-auto whitespace-pre p-3 font-code bg-white dark:bg-black text-base md:text-lg"
     v-model="state.text"
   ></textarea>
 </template>
