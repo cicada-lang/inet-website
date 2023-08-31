18ebccd20f3a3a6124f715d05151341ab3dfb97b
Xie Yuheng <xyheme@gmail.com>
2023-09-01 03:17:27 +0800

    up

--- src/components/play/mouse/trackMouse.ts
+++ src/components/play/mouse/trackMouse.ts
@@ -3,19 +3,25 @@ import { State } from '../State'
 export function trackMouse(state: State) {
   state.canvas.addEventListener('mousedown', (event) => {
     event.preventDefault()
+
     state.mouse.isDown = true
   })
 
   state.canvas.addEventListener('mouseup', (event) => {
     event.preventDefault()
+
     state.mouse.isDown = false
   })
 
   state.canvas.addEventListener('mousemove', (event) => {
+    event.preventDefault()
+
     state.mouse.position = [event.offsetX, event.offsetY]
   })
 
   state.canvas.addEventListener('touchstart', (event) => {
+    event.preventDefault()
+
     const offsetX =
       event.touches[0].pageX - (event.touches[0].target as any).offsetLeft
     const offsetY =
@@ -26,10 +32,14 @@ export function trackMouse(state: State) {
   })
 
   state.canvas.addEventListener('touchend', (event) => {
+    event.preventDefault()
+
     state.mouse.isDown = false
   })
 
   state.canvas.addEventListener('touchmove', (event: TouchEvent) => {
+    event.preventDefault()
+
     const offsetX =
       event.touches[0].pageX - (event.touches[0].target as any).offsetLeft
     const offsetY =
