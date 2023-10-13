c8668bc233d1e82cefac65179c009e840f801199
Xie Yuheng <xyheme@gmail.com>
2023-10-13 22:53:09 +0800

    quit using stateReactive

--- src/pages/playground/stateLoadReactive.ts
+++ src/pages/playground/stateLoadReactive.ts
@@ -1,17 +1,13 @@
+import { reactive } from 'vue'
 import { State } from './State'
 import { StateOptions, stateLoad } from './stateLoad'
-import { stateReactive } from './stateReactive'
 
 let cache: State | undefined = undefined
 
 export async function stateLoadReactive(options: StateOptions): Promise<State> {
-  if (cache) {
-    return cache
-  }
+  if (cache) return cache
 
-  const state = stateReactive(await stateLoad(options))
-
+  const state = reactive(await stateLoad(options))
   cache = state
-
   return state
 }
