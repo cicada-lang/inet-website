ba1920eb2c6b26e01ebdb878a7e55835006920bb
Xie Yuheng <xyheme@gmail.com>
2024-11-01 15:30:44 +0800

    up

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -40,8 +40,8 @@ onMounted(async () => {
               target="_blank"
               :href="
                 lang.isZh()
-                  ? 'https://readonly.link/articles/https://code-of-inet-js.fidb.app/docs/articles/反应网编程.md'
-                  : 'https://readonly.link/articles/https://code-of-inet-js.fidb.app/docs/articles/programming-with-interaction-nets.md'
+                  ? 'https://readonly.link/articles/https://code-of-inet-js.xieyuheng.com/docs/articles/反应网编程.md'
+                  : 'https://readonly.link/articles/https://code-of-inet-js.xieyuheng.com/docs/articles/programming-with-interaction-nets.md'
               "
             >
               <Lang>
